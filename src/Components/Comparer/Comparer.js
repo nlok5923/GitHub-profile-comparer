c9f9639ad76d6bf8eb394e6c89703c9f7e8ccfb0
Creator5923 <nlokhande5923@gmail.com>
2020-10-25 02:55:49 -0700

    improved somewhat

--- src/Components/Comparer/Comparer.js
+++ src/Components/Comparer/Comparer.js
@@ -1,65 +1,110 @@
-import React, { Component } from 'react'
-import './Comparer.css'
+import React, { Component } from 'react';
+import './Comparer.css';
 
 class Comparer extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            img_1:"./Images/prof.jpg",
-            img_2:"./Images/prof.jpg"
-        }
-        // this.setState({
-        //             img_1:this.props.usr_1,
-        //              img_2:this.props.usr_2
-        //          })
-  
-    }
-    // componentWillUpdate(){
-    //     this.setState({
-    //         img_1:this.props.usr_1,
-    //         img_2:this.props.usr_2
-    //     })
-    // }
-    
-    render() {
-        const {repos_1, follower_1,repos_2, follower_2,usr_1,usr_2,star_1,star_2} = this.props
-        // const {img_1,img_2} = this.state
-        return (
-            
-            <div>
-            <div className="Comp__pg">
-            <h1 className="res">Comparison</h1>
-            <div className="Comp__holder">
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      img_1: './Images/prof.jpg',
+      img_2: './Images/prof.jpg',
+    };
+    // this.setState({
+    //             img_1:this.props.usr_1,
+    //              img_2:this.props.usr_2
+    //          })
+  }
+  // componentWillUpdate(){
+  //     this.setState({
+  //         img_1:this.props.usr_1,
+  //         img_2:this.props.usr_2
+  //     })
+  // }
+//    componentDidUpdate(){
+//        this.setState({
+//    max_val_f : Math.max(this.props.followers_1,this.props.followers_2) + 20,
+//     max_val_fo : Math.max(this.props.forks_1,this.props.forks_2) + 20,
+//     max_val_r  : Math.max(this.props.repos_1,this.props.repos_2)+ 30,
+//     max_val_st : Math.max(this.props.count_1,this.props.count_2)+20,
+//     max_val_wa  :Math.max(this.props.watchers_1,this.props.watchers_2) + 20
+//        })
+//    }
+  render() {
+    const {
+      repos_1,
+      follower_1,
+      repos_2,
+      follower_2,
+      usr_1,
+      usr_2,
+      star_1,
+      star_2,
+      forks_1,
+      forks_2,
+      watchers_1,
+      watchers_2,
+      max_val_f,
+      max_val_r,
+      max_val_fo,
+      max_val_wa,
+      max_val_st,
+    } = this.props;
+    // const {img_1,img_2} = this.state
+    return (
+      <div>
+        <div className="Comp__pg">
+          <h1 className="res">Comparison</h1>
+          <div className="Comp__holder">
             <div className="usr__1">
-            <img src={usr_1} alt="usr_1_avatar"></img>
-            <h1>Repos</h1>
-            <meter value={repos_1} min="0" max="100"></meter>
-            <h1>Stars</h1>
-            <meter value={star_1} min="0" max="100"></meter>
-            <h1>Followers</h1>
-            <meter  value={follower_1} min="0" max="100"></meter>
-            {/* <h1>Contribution</h1>
+            {/* <h1> {max_val_f},
+      {max_val_r},
+      {max_val_fo},
+      {max_val_wa},
+      {max_val_st},</h1> */}
+              <img src={usr_1} alt="usr_1_avatar"></img>
+              <h1>Repos</h1>
+              <h1>{repos_1}</h1>
+              <meter value={repos_1} min="0" max={max_val_r}></meter>
+              <h1>Stars</h1>
+              <h1>{star_1}</h1>
+              <meter value={star_1} min="0" max={max_val_st}></meter>
+              <h1>Followers</h1>
+              <h1>{follower_1}</h1>
+              <meter value={follower_1} min="0" max={max_val_f}></meter>
+              <h1>Forks</h1>
+              <h1>{forks_1}</h1>
+              <meter value={forks_1} min="0" max={max_val_fo}></meter>
+              <h1>Watchers</h1>
+              <h1>{watchers_1}</h1>
+              <meter value={watchers_1} min="0" max={max_val_wa}></meter>
+              {/* <h1>Contribution</h1>
             <meter value="2" min="0" max="10">2 out of 10</meter> */}
             </div>
             <div className="usr__2">
-            <img src={usr_2}  alt="usr_2_avatar" ></img>
-            <h1>Repos</h1>
-            <meter  value={repos_2} min="0" max="100"></meter>
-            <h1>Stars</h1>
-            <meter  value={star_2} min="0" max="100"></meter>
-            <h1>Followers</h1>
-            <meter  value={follower_2} min="0" max="100"></meter>
-            {/* <h1>Contribution</h1>
+              <img src={usr_2} alt="usr_2_avatar"></img>
+              <h1>Repos</h1>
+              <h1>{repos_2}</h1>
+              <meter value={repos_2} min="0" max={max_val_r}></meter>
+              <h1>Stars</h1>
+              <h1>{star_2}</h1>
+              <meter value={star_2} min="0" max={max_val_st}></meter>
+              <h1>Followers</h1>
+              <h1>{follower_2}</h1>
+              <meter value={follower_2} min="0" max={max_val_f}></meter>
+              <h1>Forks</h1>
+              <h1>{forks_2}</h1>
+              <meter value={forks_2} min="0" max={max_val_fo}></meter>
+              <h1>Watchers</h1>
+              <h1>{watchers_2}</h1>
+              <meter value={watchers_2} min="0" max={max_val_wa}></meter>
+              {/* <h1>Contribution</h1>
             <meter  value="2" min="0" max="10">2 out of 10</meter> */}
             </div>
-
-            </div>
-                
-            </div>
-            </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Comparer
+export default Comparer;
--- src/Components/FrontPage/FrontPage.js
+++ src/Components/FrontPage/FrontPage.js
@@ -1,246 +1,305 @@
-import React, { Component } from 'react'
-import "./FrontPage.css"
-import axios from 'axios'
-import Comparer from "../Comparer/Comparer"
-import Contact from "../Contact/Contact"
+import React, { Component } from 'react';
+import './FrontPage.css';
+import axios from 'axios';
+import Comparer from '../Comparer/Comparer';
+import Contact from '../Contact/Contact';
 
 class FrontPage extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             usr__1:"",
-             usr__2:"",
-             usr__1__imgSrc:"./Images/prof.jpg",
-             usr__2__imgSrc:"./Images/prof.jpg",
-             no_of_repos_1:0,
-             no_of_followers_1:0,
-             no_of_gists_1:0,
-             no_of_repos_2:0,
-             no_of_followers_2:0,
-             no_of_gists_2:0,
-             max_val_f:0,
-             max_val_r:0,
-             repos_1:[],
-             repos_2:[],
-             rep:"",
-             count_1:0,
-             count_2:0
-        }
-    }
-    handleUsr__1=(e)=>{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      usr__1: '',
+      usr__2: '',
+      usr__1__imgSrc: './Images/prof.jpg',
+      usr__2__imgSrc: './Images/prof.jpg',
+      no_of_repos_1: 0,
+      no_of_followers_1: 0,
+      no_of_gists_1: 0,
+      no_of_repos_2: 0,
+      no_of_followers_2: 0,
+      no_of_gists_2: 0,
+      repos_1: [],
+      repos_2: [],
+      rep: '',
+      count_1: 0,
+      count_2: 0,
+      forks_1:0,
+      forks_2:0,
+      watchers_1:0,
+      watchers_2:0,
+      max_val_f:0,
+      max_val_r:0,
+      max_val_fo:0,
+      max_val_wa:0,
+      max_val_st:0
+    };
+  }
+  handleUsr__1 = (e) => {
+    this.setState({
+      usr__1: e.target.value,
+    });
+  };
+  handleUsr__2 = (e) => {
+    this.setState({
+      usr__2: e.target.value,
+    });
+  };
+  handleChangeU1 = () => {
+    // this.setState({[e.target.name]:e.target.value})
+    //   console.log(`https://api.github.com/users/${this.state.usr__1}`)
+    axios
+      .get(`https://api.github.com/users/${this.state.usr__1}`)
+      .then((response) => {
         this.setState({
-            usr__1:e.target.value
-        })
-    }
-    handleUsr__2=(e)=>{
+          usr__1__imgSrc: response.data.avatar_url,
+          no_of_repos_1: response.data.public_repos,
+          no_of_followers_1: response.data.followers,
+          no_of_gists_1: response.data.public_gists,
+        });
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("please put a correct username")
+        this.setState({ errorMsg: 'Error retriveing data' });
+      });
+    axios
+      .get(`https://api.github.com/users/${this.state.usr__1}/repos`)
+      .then((response) => {
+        console.log(response);
         this.setState({
-            usr__2:e.target.value
-        })
-    }
-    handleChangeU1=()=>{
+          repos_1: response.data,
+        });
+        //stargazers_count: 0
+        //console.log(this.state.repos_1[0])
+        //   var coun = 0 ;
 
-       // this.setState({[e.target.name]:e.target.value})
-     //   console.log(`https://api.github.com/users/${this.state.usr__1}`)
-            axios.get(`https://api.github.com/users/${this.state.usr__1}`)
-            .then(response =>{
-                this.setState({
-                    usr__1__imgSrc:response.data.avatar_url,
-                    no_of_repos_1:response.data.public_repos,
-                    no_of_followers_1:response.data.followers,
-                    no_of_gists_1:response.data.public_gists
-                   })
-                console.log(response)
-            })
-            .catch(error =>{
-                console.log(error);
-                this.setState({errorMsg:"Error retriveing data"})
-            })
-            axios.get(`https://api.github.com/users/${this.state.usr__1}/repos`)
-            .then(response=>{
-                console.log(response)
-                this.setState({
-                    repos_1:response.data,
-                })
-                //stargazers_count: 0
-                //console.log(this.state.repos_1[0])
-             //   var coun = 0 ;
-               
-            this.state.repos_1.map(repo => {
-                this.setState(()=>({
-                    count_1 :this.state.count_1 + repo.stargazers_count
-                }))
-                return console.log(repo.stargazers_count)
-               // console.log(this.state.count)
-             })
-              //  console.log(Number(coun))
-                
-               // console.log(this.state.count)
+        this.state.repos_1.map((repo) => {
+          this.setState(() => ({
+            count_1: this.state.count_1 + repo.stargazers_count,
+            forks_1:this.state.forks_1 + repo.forks_count,
+            watchers_1:this.state.watchers_1 + repo.watchers_count
+          }));
+          return console.log(repo.stargazers_count);
+          // console.log(this.state.count)
+        });
+        //  console.log(Number(coun))
 
-                
-            })
+        // console.log(this.state.count)
+      });
 
-           //  console.log(this.state.repos_1[0])
-            //http://api.github.com/repos/[username]/[reponame]
-           // https://api.github.com/users/nlok5923/starred{/owner}{/repo}
-            // axios.get(`http://api.github.com/repos/${this.state.usr__1}/${this.state.rep}`)
-            // .then(response=>{
-            //     console.log(response)
-            //     this.setState({
-            //         repos_1:response.data
-            //     })
-            // })
-          
-        // if(this.state.use__2!=''){
-        //     axios.get(`https://api.github.com/users/${this.state.usr__2}`)
-        //     .then(response =>{
-        //         console.log(response)
-        //     })
-        //     .catch(error =>{
-        //         console.log(error);
-        //         this.setState({errorMsg:"Error retriveing data"})
-        //     })
-       
-        // }
-        
-
-    }
-    handleClick=()=>{
-    let k =  document.getElementById("front__page")
-    k.style.display='none';
-    let g =  document.getElementById("comp")
-    g.style.display="block"
-    // if(this.state.follower_1 > this.state.follower_2){
+    //  console.log(this.state.repos_1[0])
+    //http://api.github.com/repos/[username]/[reponame]
+    // https://api.github.com/users/nlok5923/starred{/owner}{/repo}
+    // axios.get(`http://api.github.com/repos/${this.state.usr__1}/${this.state.rep}`)
+    // .then(response=>{
+    //     console.log(response)
     //     this.setState({
-    //         max_val:this.state.follower_1+100
+    //         repos_1:response.data
     //     })
-    // }
-    // else{
-    //     this.setState({
-    //         max_val_f:this.state.follower_2+100
-    //     })
-    // }
-
-    // if(this.state.repos_1 > this.state.repos_2){
-    //     this.setState({
-    //         max_val_r:this.state.repos_1+100
-    //     })
-    // }
-    // else{
-    //     this.setState({
-    //         max_val_r:this.state.repos_2+100
-    //     })
-    // }
-    // this.setState({
-    //     img_1:this.state.usr__1__imgSrc,
-    //     img_2:this.state.usr__2__imgSrc
     // })
 
+    // if(this.state.use__2!=''){
+    //     axios.get(`https://api.github.com/users/${this.state.usr__2}`)
+    //     .then(response =>{
+    //         console.log(response)
+    //     })
+    //     .catch(error =>{
+    //         console.log(error);
+    //         this.setState({errorMsg:"Error retriveing data"})
+    //     })
 
-    }
-    handleChangeU2=()=>{
+    // }
+  };
+  handleClick = () => {
 
-        // this.setState({[e.target.name]:e.target.value})
-      //   console.log(`https://api.github.com/users/${this.state.usr__1}`)
-    //  console.log("called")
-             axios.get(`https://api.github.com/users/${this.state.usr__2}`)
-             .then(response =>{
-                 //console.log(response)
-               this.setState({
-                no_of_followers_2:response.data.followers,
-                no_of_repos_2:response.data.public_repos,
-                no_of_gists_2:response.data.public_gists,
-                usr__2__imgSrc:response.data.avatar_url,
-               })
-               console.log(this.state.usr__2__imgSrc)
-                 
-             })
-             .catch(error =>{
-                 console.log(error);
-                 this.setState({errorMsg:"Error retriveing data"})
-             })
-
-             axios.get(`https://api.github.com/users/${this.state.usr__2}/repos`)
-             .then(response=>{
-                 console.log(response)
-                 this.setState({
-                     repos_2:response.data,
-                 })
-                 //stargazers_count: 0
-                 //console.log(this.state.repos_1[0])
-              //   var coun = 0 ;
-                
-              this.state.repos_2.map(repo => {
-                 this.setState(()=>({
-                     count_2 :this.state.count_2 + repo.stargazers_count
-                 }))
-               return  console.log(repo.stargazers_count)
-                // console.log(this.state.count)
-              })
-               //  console.log(Number(coun))
-                 
-                // console.log(this.state.count)
- 
-                 
-             })
- 
-           
-         // if(this.state.use__2!=''){
-         //     axios.get(`https://api.github.com/users/${this.state.usr__2}`)
-         //     .then(response =>{
-         //         console.log(response)
-         //     })
-         //     .catch(error =>{
-         //         console.log(error);
-         //         this.setState({errorMsg:"Error retriveing data"})
-         //     })
-        
-         // }
-         
- 
+     if(this.state.usr__1==='' || this.state.usr__2 ===''){
+         alert("please fill all fields");
      }
-    
-    render() {
-        const {usr__1,usr__2, count_1,count_2,usr__1__imgSrc,usr__2__imgSrc, no_of_repos_1 , no_of_followers_1, no_of_gists_1,no_of_repos_2 , no_of_followers_2, no_of_gists_2} = this.state
+     else
+     {
 
-        return (
-            <div>
-            <div id="front__page">
-            <div className="whole__pg">
+    let k = document.getElementById('front__page');
+    k.style.display = 'none';
+    let g = document.getElementById('comp');
+    g.style.display = 'block';
+    this.setState({
+   max_val_f : Math.max(this.state.no_of_followers_1,this.state.no_of_followers_2) + 20,
+  max_val_fo : Math.max(this.state.forks_1,this.state.forks_2) + 20,
+   max_val_r : Math.max(this.state.no_of_repos_1,this.state.no_of_repos_2)+ 30,
+   max_val_st : Math.max(this.state.count_1,this.state.count_2)+20,
+    max_val_wa : Math.max(this.state.watchers_1,this.state.watchers_2) + 20
+    })
+    }
+  };
+  handleChangeU2 = () => {
+    // this.setState({[e.target.name]:e.target.value})
+    //   console.log(`https://api.github.com/users/${this.state.usr__1}`)
+    //  console.log("called")
+    axios
+      .get(`https://api.github.com/users/${this.state.usr__2}`)
+      .then((response) => {
+        //console.log(response)
+        this.setState({
+          no_of_followers_2: response.data.followers,
+          no_of_repos_2: response.data.public_repos,
+          no_of_gists_2: response.data.public_gists,
+          usr__2__imgSrc: response.data.avatar_url,
+        });
+        console.log(this.state.usr__2__imgSrc);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("please put a correct username")
+        this.setState({ errorMsg: 'Error retriveing data' });
+      });
+
+    axios
+      .get(`https://api.github.com/users/${this.state.usr__2}/repos`)
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          repos_2: response.data
+        });
+        //stargazers_count: 0
+        //console.log(this.state.repos_1[0])
+        //   var coun = 0 ;
+
+        this.state.repos_2.map((repo) => {
+          this.setState(() => ({
+            count_2: this.state.count_2 + repo.stargazers_count,
+            watchers_2:this.state.watchers_1 + repo.watchers_count,
+            forks_2:this.state.forks_2 + repo.forks_count
+          }));
+          return console.log(repo.stargazers_count);
+          // console.log(this.state.count)
+        });
+        //  console.log(Number(coun))
+
+        // console.log(this.state.count)
+      });
+
+    // if(this.state.use__2!=''){
+    //     axios.get(`https://api.github.com/users/${this.state.usr__2}`)
+    //     .then(response =>{
+    //         console.log(response)
+    //     })
+    //     .catch(error =>{
+    //         console.log(error);
+    //         this.setState({errorMsg:"Error retriveing data"})
+    //     })
+
+    // }
+  };
+
+  render() {
+    const {
+      usr__1,
+      usr__2,
+      count_1,
+      count_2,
+      usr__1__imgSrc,
+      usr__2__imgSrc,
+      no_of_repos_1,
+      no_of_followers_1,
+      no_of_gists_1,
+      no_of_repos_2,
+      no_of_followers_2,
+      no_of_gists_2,
+      forks_1,
+      forks_2,
+      watchers_1,
+      watchers_2,
+      max_val_f,
+      max_val_r,
+      max_val_fo,
+      max_val_wa,
+      max_val_st
+    } = this.state;
+
+    return (
+      <div>
+        <div id="front__page">
+          <div className="whole__pg">
             <h1 className="main__head">GitHub Profile Comparer </h1>
             <div className="main__hold">
-            <div className="user__cred">
-            <div className="input__1">
-            <img src={usr__1__imgSrc} className="avatar__img" alt="you_avatar"></img>
-            <h1 className="you">You</h1>
-            <input type="text" name={usr__1} onChange={this.handleUsr__1}></input>
-            <button className="nxt__btn" onClick={this.handleChangeU1} >&#8594;</button>
+              <div className="user__cred">
+                <div className="input__1">
+                  <img
+                    src={usr__1__imgSrc}
+                    className="avatar__img"
+                    alt="you_avatar"
+                  ></img>
+                  <h1 className="you">You</h1>
+                  <input
+                    type="text"
+                    name={usr__1}
+                    placeholder='username'
+                    onChange={this.handleUsr__1}
+                  ></input>
+                  <button className="nxt__btn" onClick={this.handleChangeU1}>
+                    &#8594;
+                  </button>
+                </div>
+                <div className="bound"></div>
+                <div className="input__2">
+                  <img
+                    src={usr__2__imgSrc}
+                    className="avatar__img"
+                    alt="you_avatar"
+                  ></img>
+                  <h1 className="friend">Friend</h1>
+                  <input
+                    type="text"
+                    value={usr__2}
+                    name={usr__2}
+                    placeholder="username"
+                    onChange={this.handleUsr__2}
+                  ></input>
+                  <button className="nxt__btn" onClick={this.handleChangeU2}>
+                    &#8594;
+                  </button>
+                </div>
+              </div>
+              <button className="next__btn" onClick={this.handleClick}>
+                NEXT
+              </button>
             </div>
-            <div className="bound">
-            </div>
-            <div className="input__2">
-            <img src={usr__2__imgSrc} className="avatar__img" alt="you_avatar"></img>
-            <h1 className="friend">Friend</h1>
-            <input type="text" value={usr__2} name={usr__2} onChange={this.handleUsr__2} ></input>
-            <button className="nxt__btn" onClick={this.handleChangeU2}>&#8594;</button>
-            </div>
-            </div>
-            <button className="next__btn" onClick={this.handleClick}>NEXT</button>
-            </div>
-            </div>
-            
-            {/* <Comparer /> */}
-            </div>
-            {/* no_of_followers:response.data.followers,
+          </div>
+
+          {/* <Comparer /> */}
+        </div>
+        {/* no_of_followers:response.data.followers,
                 no_of_repos:response.data.public_repos,
                 no_of_gists:response.data.public_gists */}
-                <Contact />
-            <div id="comp"> 
-                <Comparer repos_1={no_of_repos_1} follower_1={no_of_followers_1} gists_1={no_of_gists_1} repos_2={no_of_repos_2} follower_2={no_of_followers_2} gists_2={no_of_gists_2} usr_1={usr__1__imgSrc} usr_2={usr__2__imgSrc} star_1={count_1} star_2={count_2}   />
-             </div> 
-            </div>
-        )
-    }
+        <Contact />
+        <div id="comp">
+          <Comparer
+            repos_1={no_of_repos_1}
+            follower_1={no_of_followers_1}
+            gists_1={no_of_gists_1}
+            repos_2={no_of_repos_2}
+            follower_2={no_of_followers_2}
+            gists_2={no_of_gists_2}
+            usr_1={usr__1__imgSrc}
+            usr_2={usr__2__imgSrc}
+            star_1={count_1}
+            star_2={count_2}
+            forks_1={forks_1}
+            forks_2={forks_2}
+            watchers_1={watchers_1}
+            watchers_2={watchers_2}
+            max_val_f ={max_val_f}
+            max_val_r={ max_val_r}
+            max_val_fo={ max_val_fo} 
+            max_val_wa ={max_val_wa}
+            max_val_st = {max_val_st}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default FrontPage
+export default FrontPage;
